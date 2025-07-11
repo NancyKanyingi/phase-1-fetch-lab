6aa532f20ad30ed3fb5a8b1952704f010b77aac2
Nancy <wangui.kanyingi2015@gmail.com>
2025-07-11 15:15:42 +0300

    Adds index.js

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
-}
+ function fetchBooks(){
+ fetch('https://anapioficeandfire.com/api/books')
+      .then(function(response){
+        return response.json();
+      } )
+      .then(function(data){
+        //console.log(data)
+        renderBooks(data)
+      })
+    }
+    
 
 function renderBooks(books) {
   const main = document.querySelector('main');
@@ -13,5 +20,5 @@ function renderBooks(books) {
 }
 
 document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+      fetchBooks()
 });
